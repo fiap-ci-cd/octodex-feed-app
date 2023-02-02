454edd43ca9c9ba4a55cc251866c35c1423959eb
Pedro Lacerda <profpedro.lacerda@fiap.com.br>
2023-02-02 16:19:28 -0300

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   darkMode: 'class',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    typography: (theme) => ({}),
-    extend: {},
-  },
-  variants: {},
-  plugins: [require('@tailwindcss/typography')],
+   darkMode: class,
+   theme: {
+     extend: {},
+   },
+   variants: {
+     extend: {},
+   },
+   plugins: [require('@tailwindcss/typography')],
 }
