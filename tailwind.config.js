9d4f6170c8d3dabf84423a5eda171cea5ded5205
Pedro Lacerda <profpedro.lacerda@fiap.com.br>
2023-02-02 16:23:19 -0300

    fix: tailwind duplicates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 module.exports = {
   darkMode: 'class',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-   darkMode: class,
-   theme: {
-     extend: {},
+  theme: {
+    extend: {},
    },
-   variants: {
-     extend: {},
+  variants: {
+    extend: {},
    },
-   plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography')],
 }
