4214cfd151fa88de47f5e399208d2635c5247ce7
Pedro Lacerda <609076+pedrolacerda@users.noreply.github.com>
2023-02-07 19:52:00 +0000

    feat: Add dark background

--- components/octodexHeader.js
+++ components/octodexHeader.js
@@ -1,14 +1,12 @@
 import Image from "next/image";
 import {Box, Grid, Typography} from "@mui/material";
-import useTheme from "next-themes";
-import {MoonIcon, SunIcon} from "@heroicons/react/solid";
 
 const iconSize = 64;
 
 export default function OctodexHeader() {
 
   return (
-    <header className="h-15 shadow-sm dark:bg-gray-700 bg-white">"
+    <header>
       <Box className="header">
         <Grid container spacing={2}>
           <Grid item>
