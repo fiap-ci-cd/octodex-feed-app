023d5ad943d2a89ef970cbd5b952f43cbbce9d0c
Pedro Lacerda <pedrolacerda@github.com>
2023-02-08 15:27:37 -0300

    add dark-mode task

--- components/octodexHeader.js
+++ components/octodexHeader.js
@@ -1,38 +1,9 @@
 import Image from "next/image";
 import {Box, Grid, Typography} from "@mui/material";
-import {useTheme} from "next-themes";
-import {useState, useEffect} from "react";
-import {MoonIcon, SunIcon} from "@heroicons/react/solid";
 
 const iconSize = 64;
 
 export default function OctodexHeader() {
-
-  const {systemTheme , theme, setTheme} = useTheme();
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() =>{
-    setMounted(true);
-  },[])
-
-  const renderThemeChanger = () => {
-
-    if(!mounted) return null;
-
-    const currentTheme = theme === "light" ? systemTheme : theme ;
-
-    if(currentTheme ==="dark"){
-      return (
-        <SunIcon className="w-10 h-10 text-yellow-500 " role="button" onClick={() => setTheme('light')} />
-      )
-    }
-
-    else {
-      return (
-        <MoonIcon className="w-10 h-10 text-gray-900 " role="button" onClick={() => setTheme('dark')} />
-      )
-    }
-  };
 
   return (
       <Box className="header">
