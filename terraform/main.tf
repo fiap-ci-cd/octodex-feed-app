88b72fda58d39f44eba54f3f77a1b9037e130dc1
Pedro Lacerda <pedrolacerda@github.com>
2023-02-23 20:02:00 -0300

    try some improvements

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,15 +21,15 @@ resource "azurerm_resource_group" "rg" {
 # Create the Linux App Service Plan
 resource "azurerm_service_plan" "appserviceplan" {
   name                = var.app_service_plan_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "S1"
 }
 
 resource "azurerm_linux_web_app" "webapp" {
   name                = var.app_service_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_service_plan.appserviceplan.resource_group_name
   location            = azurerm_service_plan.appserviceplan.location
   service_plan_id     = azurerm_service_plan.appserviceplan.id
 
